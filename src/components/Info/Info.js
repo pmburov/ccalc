6be406a7b60d7e794f782fb10da0d4e139ac51c0
Pavel Burov <p.burov@corp.mail.ru>
2019-06-05 16:50:33 +0300

    info

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -4,8 +4,18 @@ import React from 'react';
 const Info = () => {
   return (
     <div>
-      <p>Formula:</p>
+      <h4>Formula</h4>
       <code>(655 + (9.6 * weight) + (1.8 * height) - (4.7 * age)) * activity</code>
+      <h4>Activity</h4>
+      <p>What to choose?</p>
+      <h5>1.20</h5>
+      <p>Office life, no gym or other sports</p>
+      <h5>1.38</h5>
+      <p>1-3 iight workouts per week (e.g. at home)</p>
+      <h5>1.55</h5>
+      <p>3-5 moderate workouts per week (e.g. group aerobics)</p>
+      <h5>1.73</h5>
+      <p>5-7 intensive workouts per week</p>
     </div>
   );
 }
