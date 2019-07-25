e7305d4cbea31859dd4f0ffe2ee921be81ceb823
Pavel <darkdelphin@gmail.com>
2019-07-25 10:57:28 +0300

    doughnut chart prototype

--- src/components/Calc/Calc.js
+++ src/components/Calc/Calc.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import styled, { css } from 'styled-components/macro';
 import useLocalStorage from 'react-use/lib/useLocalStorage';
+import Chart from "chart.js";
 
 import { calcDaily, calcGain, calcLoss } from '../../core/calc';
 
@@ -10,14 +11,56 @@ const Calc = () => {
   const [age, setAge] = useLocalStorage('age', 0);
   const [activity, setActivity] = useLocalStorage('activity', 1.20);
   const [alterPercent, setAlterPercent] = useLocalStorage('alterPercent', 10);
+  const chartRef = useRef(null);
 
   const daily = calcDaily(weight, height, age, activity);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      const myChartRef = chartRef.current.getContext("2d");
+
+      const carbohydrates = daily*50/100;
+      const proteins = daily*20/100;
+      const fats = daily*30/100;
+
+      const data = {
+        datasets: [{
+            // data: [50, 20, 30],
+            data: [carbohydrates.toFixed(0), proteins.toFixed(0), fats.toFixed(0)],
+            backgroundColor: [
+              '#4dc9f6',
+              '#f67019',
+              '#f53794',
+            ],
+        }],
+
+
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+          'Carbohydrates',
+          'Proteins',
+          'Fats'
+        ]
+      };
+
+      new Chart(myChartRef, {
+        type: 'doughnut',
+        data: data,
+        options: {
+          legend: {
+            display: true,
+            position: 'bottom'
+          }
+        }
+      });
+    }
+  }, [weight, height, age, daily]);
 
   return (
     <Section>
       <Block>
         <Label htmlFor="weight">Weight</Label>
-        <Input id="weight" type="number" value={weight} onChange={(e) => setWeight(e.target.value)} />
+        <Input id="weight" type="number" value={weight} onChange={(e) => setWeight(e.target.value)} autoFocus />
       </Block>
       <Block>
         <Label htmlFor="height">Height</Label>
@@ -55,6 +98,11 @@ const Calc = () => {
         Gain {calcGain(daily, alterPercent)} <br />
         Loss {calcLoss(daily, alterPercent)}
       </Block>
+      {weight && height && age && (
+        <Block>
+          <canvas id="myChart" ref={chartRef} />
+        </Block>
+      )}
     </Section>
   );
 };
@@ -77,6 +125,7 @@ const Select = styled.select`
 `;
 
 const Block = styled.div`
+  position: relative;
   margin-bottom: 24px;
 `;
 
