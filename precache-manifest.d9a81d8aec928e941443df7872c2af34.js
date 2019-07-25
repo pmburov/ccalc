fe60e71df7c44b32042171f6356795e1906d900c
Pavel <darkdelphin@gmail.com>
2019-07-25 11:00:36 +0300

    Updates

---
+++ precache-manifest.d9a81d8aec928e941443df7872c2af34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78be219abd5f60fc507254ddd8ccdb6a",
+    "url": "/ccalc/index.html"
+  },
+  {
+    "revision": "c4b6030fb4b784c1f14a",
+    "url": "/ccalc/static/css/main.798c9cc5.chunk.css"
+  },
+  {
+    "revision": "7d45d13718d01e008691",
+    "url": "/ccalc/static/js/2.24cb88a4.chunk.js"
+  },
+  {
+    "revision": "c4b6030fb4b784c1f14a",
+    "url": "/ccalc/static/js/main.6649d536.chunk.js"
+  },
+  {
+    "revision": "dada374cea94f559a2be",
+    "url": "/ccalc/static/js/runtime~main.9c2f42ed.js"
+  }
+]);
